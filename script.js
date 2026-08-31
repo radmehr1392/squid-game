7475e2c203d963f4382f1b90e0fe0161cc3609ce
radmehr1392 <rad50739@gmail.com>
2026-08-31 22:28:19 +0330

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,34 @@ function copyIP() {
         });
 }
 
+document.querySelectorAll(".download-button").forEach(button => {
+
+    button.addEventListener("click", function () {
+
+        const alertBox = document.createElement("div");
+
+        alertBox.className = "download-alert";
+
+        alertBox.innerHTML = `
+            <span>✓</span>
+            مود دانلود شد!
+        `;
+
+        document.body.appendChild(alertBox);
+
+        setTimeout(() => {
+            alertBox.classList.add("show");
+        }, 10);
+
+        setTimeout(() => {
+            alertBox.classList.remove("show");
+
+            setTimeout(() => {
+                alertBox.remove();
+            }, 400);
+
+        }, 2500);
+
+    });
+
+});
